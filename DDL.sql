ad6e7b3f0946c48079246d0316d58455be30a13a
JA-Lourenco <ja.lourenco@outlook.com>
2022-09-20 21:30:11 -0300

    creating more tables

--- DDL.sql
+++ DDL.sql
@@ -39,7 +39,7 @@ CREATE TABLE team(
 CREATE TABLE team_has_mechanic(
 	idTHM_mechanic INT,
     idTHM_team INT,
-    
+    teamLeader VARCHAR(50) NOT NULL,
     PRIMARY KEY(idTHM_mechanic, idTHM_team),
     CONSTRAINT fk_THM_mechanic FOREIGN KEY(idTHM_mechanic) REFERENCES mechanic(idMechanic),
     CONSTRAINT fk_THM_team FOREIGN KEY(idTHM_team) REFERENCES team(idTeam)
@@ -61,7 +61,7 @@ CREATE TABLE serviceOrder(
     idSO_vehicle INT,
     idSO_team INT,
     numberSO INT NOT NULL,
-    statusSO ENUM('') NOT NULL,
+    statusSO ENUM('Processing', 'Reviewing', 'Troubleshooting ', 'Complete', 'Available') DEFAULT 'Processing',
     emissionDate DATE NOT NULL,
     completionDate DATE NOT NULL,
     valueSO FLOAT NOT NULL,
@@ -73,14 +73,47 @@ CREATE TABLE serviceOrder(
 );
 
 CREATE TABLE service(
-	idService INT AUTO_INCREMENT PRIMARY KEY
+	idService INT AUTO_INCREMENT PRIMARY KEY,
+    serviceName VARCHAR(50) NOT NULL,
+    averageTime INT NOT NULL
 );
 
 CREATE TABLE SO_has_service(
 	idServiceOrder INT,
     idService INT,
-    typeService ENUM('Revision', 'Maintenance'),
+    typeService ENUM('Revision', 'Maintenance') NOT NULL,
+    totalAVGtime INT NOT NULL,
     PRIMARY KEY(idServiceOrder, idService),
     CONSTRAINT fk_soHservice_SO FOREIGN KEY(idServiceOrder) REFERENCES serviceOrder(idServiceOrder),
     CONSTRAINT fk_soHservice_service FOREIGN KEY(idService) REFERENCES service(idService)
+);
+
+CREATE TABLE valuesOfService(
+	idvaluesOfService INT AUTO_INCREMENT PRIMARY KEY,
+    idService INT,
+    serviceValue FLOAT NOT NULL,
+    CONSTRAINT fk_valuesOfService_service FOREIGN KEY(idService) REFERENCES service(idService)
+);
+
+CREATE TABLE SO_has_valuesService(
+	idServiceOrder INT,
+	idvaluesOfService INT,
+    PRIMARY KEY (idServiceOrder, idvaluesOfService),
+    CONSTRAINT fk_soHvaluesservice_serviceOrder FOREIGN KEY(idServiceOrder) REFERENCES serviceOrder(idServiceOrder),
+    CONSTRAINT fk_soHvaluesservice_valuesOfService FOREIGN KEY(idvaluesOfService) REFERENCES valuesOfService(idvaluesOfService)
+);
+
+CREATE TABLE components(
+	idComponent INT AUTO_INCREMENT PRIMARY KEY,
+    componentName VARCHAR(50) NOT NULL,
+    brand VARCHAR(50) NOT NULL,
+    cValue FLOAT NOT NULL
+);
+
+CREATE TABLE SO_has_component(
+	idServiceOrder INT,
+    idComponent INT,
+    quantity INT NOT NULL,
+    CONSTRAINT fk_soHcomponent_serviceOrder FOREIGN KEY(idServiceOrder) REFERENCES serviceOrder(idServiceOrder),
+    CONSTRAINT fk_soHcomponent_components FOREIGN KEY(idComponent) REFERENCES components(idComponent)
 );
